f7c72ab4d26bb9bb98daa897578c1c01db8f6814
akshaynetankar <akshaynetankar94@gmail.com>
2024-11-03 13:42:17 +0530

    Initial setup

--- src/main/java/com/shoppingMart/entities/User.java
+++ src/main/java/com/shoppingMart/entities/User.java
@@ -1,4 +1,19 @@
 package com.shoppingMart.entities;
 
+import jakarta.persistence.*;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
