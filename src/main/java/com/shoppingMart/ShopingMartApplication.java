96b2fe4b0e3851040e00d38da16682e8ee565341
akshaynetankar <akshaynetankar94@gmail.com>
2024-11-03 13:16:21 +0530

    initial setup

---
+++ src/main/java/com/shoppingMart/ShopingMartApplication.java
@@ -0,0 +1,13 @@
+package com.shoppingMart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopingMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopingMartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shoppingMart/ShopingMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoppingMart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopingMartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
